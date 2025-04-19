7e6d6dcd38b6943d3bea53eaabdb20a849af4ee8
Azra nur emeksiz <azranuremeksiz@gmail.com>
2025-04-19 17:24:59 +0200

    Show adoptable pets on the browser that is added by the admin.

--- src/routes/(frontend)/+page.svelte
+++ src/routes/(frontend)/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import type { Pet } from '$lib/types';
 
-	let pets = [];
+	
+	let pets: Pet[] = [];
 	let petType: '' | 'puppy' | 'kitten' = '';
 
 	async function loadPets() {
@@ -21,8 +23,20 @@
 {#if pets.length === 0}
     <p>No pets available.</p>
 {:else}
-    <!--    show available pets here-->
+
+<ul>
+	{#each pets as pet (pet.id)}
+		<li>
+			<strong>{pet.name}</strong> ({pet.type}) - 
+			{pet.adopted ? 'Adopted' : 'Available'}
+			{#if !pet.adopted}
+				<button on:click={() => adopt(pet.id)}>Adopt</button>
+			{/if}
+		</li>
+	{/each}
+</ul>
 {/if}
+		
 
 <style>
 </style>
