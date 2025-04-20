e089863b6c0f5c336841ec1ea123b41c8cbe01b3
Azra nur emeksiz <azranuremeksiz@gmail.com>
2025-04-20 17:21:33 +0200

    Now every users logs are shown and data is fetched from log.json

--- src/routes/(frontend)/logs/+page.svelte
+++ src/routes/(frontend)/logs/+page.svelte
@@ -4,8 +4,16 @@
 	let error = '';
 
 	onMount(async () => {
-		// TODO get logs
+		try { 
+			const res = await fetch('/api/log');
+			if (!res.ok) throw new Error('Failed to fetch logs');
+			const data = await res.json();
+			logs = data.reverse(); //to push newer logs up
+
+		}
+		catch (err) { error = 'Failed to load logs.';}
 	});
+
 </script>
 
 <h1>Action Log</h1>
@@ -15,8 +23,25 @@
 {:else if logs.length === 0}
     <p>No actions have been logged yet.</p>
 {:else}
-    <!--   render logs here with the newest on top-->
+<ul>
+	{#each logs as log}
+		<li>{log}</li>
+	{/each}
+</ul>
+
 {/if}
 
 <style>
+	h1 {
+		font-size: 2rem;
+		margin-bottom: 1rem;
+	}
+	ul {
+		list-style: none;
+		padding: 0;
+	}
+	li {
+		padding: 0.5rem 0;
+		border-bottom: 1px solid #d34ce2;
+	}
 </style>
